70c68f0ea2d4a767806389e26095be9d8af5ad70
Marton Papp <marton.papp@emarsys.com>
2019-02-10 10:39:51 +0100

    base tilemap

---
+++ Assets/Bomb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float countDown = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+
+        if(countDown <= 0f)
+        {
+            Debug.Log("Boom");
+            Destroy(gameObject);
+        }
+
+    }
+}
